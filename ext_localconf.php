8c532635cf484f3e06db3f0517563b5b31eb9569
Benjamin Riezler <b.riezler@pixel-ink.de>
2017-01-08 16:42:22 +0100

    [BUGFIX] Renamed files for naming convention purposes

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,5 +4,5 @@ if (!defined('TYPO3_MODE')) {
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:$extension_name$/Configuration/TSConfig/Page.txt">'
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:$extension_name$/Configuration/TSConfig/page.txt">'
 );
--- ext_tables.php
+++ ext_tables.php
@@ -1,1 +1,4 @@
 <?php
+if (!defined('TYPO3_MODE')) {
+    die('Access denied');
+}
